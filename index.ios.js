4980af6b83afb94798b80fcafd17e34fa2c1d760
Natalie Loh <hi@natalieloh.com>
2016-06-18 16:48:41 +0800

    Refactored items into list view

--- index.ios.js
+++ index.ios.js
@@ -8,47 +8,39 @@ import React, {
   View
 } from 'react-native';
 
-var testData = [{"firstName":"Black","lastName":"Garrett"},
-{"firstName":"Morales","lastName":"Duncan"},
-{"firstName":"Ramos","lastName":"King"},
-{"firstName":"Dunn","lastName":"Collins"},
-{"firstName":"Fernandez","lastName":"Montgomery"},
-{"firstName":"Burns","lastName":"Fox"},
-{"firstName":"Richardson","lastName":"Kim"},
-{"firstName":"Hanson","lastName":"Evans"},
-{"firstName":"Anderson","lastName":"Hunt"},
-{"firstName":"Carter","lastName":"Grant"},
-{"firstName":"Ray","lastName":"Ruiz"},
-{"firstName":"Hart","lastName":"Schmidt"},
-{"firstName":"White","lastName":"Andrews"},
-{"firstName":"Hall","lastName":"Holmes"},
-{"firstName":"Hawkins","lastName":"Gomez"},
-{"firstName":"Bowman","lastName":"Sullivan"},
-{"firstName":"Brooks","lastName":"Evans"},
-{"firstName":"Reyes","lastName":"Perez"},
-{"firstName":"Dixon","lastName":"Barnes"},
-{"firstName":"Ward","lastName":"Lee"},
-{"firstName":"Berry","lastName":"Payne"},
-{"firstName":"Murray","lastName":"Rose"},
-{"firstName":"Stephens","lastName":"Fowler"},
-{"firstName":"Rodriguez","lastName":"Lewis"},
-{"firstName":"Cook","lastName":"Dean"}];
+var itemData = [
+  {
+    "itemImage": "./img/items/kopi.png",
+    "itemName": "Kopi O",
+    "itemDescription": "1 floof / second",
+  },{
+    "itemImage": "./img/items/toast.png",
+    "itemName": "Kaya Toast",
+    "itemDescription": "2 floof / second",
+  },{
+    "itemImage": "./img/items/softboiledeggs.png",
+    "itemName": "Soft Boiled Eggs",
+    "itemDescription": "3 floof / second",
+  },{
+    "itemImage": "./img/items/softboiledeggs.png",
+    "itemName": "Soft Boiled Eggs",
+    "itemDescription": "3 floof / second",
+  },
+];
 
-function compare(a,b) {
-  if (a.lastName < b.lastName)
-    return -1;
-  if (a.lastName > b.lastName)
-    return 1;
-  return 0;
-}
-
-var SampleRow = React.createClass({
+var ItemRow = React.createClass({
   render() {
     return (
-      <View style={styles.wrapper}>
-        <View>
-          <Text style={styles.text}>{this.props.lastName}, {this.props.firstName}</Text>
-        </View>
+      <View style={styles.itemContainer}>
+        <Image source={require('./img/items/kopi.png')} style={styles.itemThumbnail} />
+        
+        <Text style={styles.itemTitle}>
+          {this.props.itemName}
+        </Text>
+        
+        <Text style={styles.itemDescription}>
+          {this.props.itemDescription}
+        </Text>
       </View>
     );
   }
@@ -56,77 +48,40 @@ var SampleRow = React.createClass({
 
 var ListViewRowsAndSections = React.createClass({
   getInitialState: function() {
-    var ds = new ListView.DataSource({
-      sectionHeaderHasChanged: (r1, r2) => r1 !== r2,
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     
-   	var {data, sectionIds} = this.renderListViewData(testData.sort(compare));
-
     return {
-      dataSource: ds.cloneWithRowsAndSections(data, sectionIds)
+      dataSource: ds.cloneWithRows(itemData),
     };
   },
   
-  componentDidMount() {
-    var listViewScrollView = this.refs.listView.getScrollResponder();
-    // listViewScrollView.scrollTo(1); // Hack to get ListView to render fully
-  },
-  
-  renderListViewData(users) {
-    var data = {};
-    var sectionIds = [];
-    
-    users.map((user) => {
-      var section = user.lastName.charAt(0);
-      if (sectionIds.indexOf(section) === -1) {
-        sectionIds.push(section);
-        data[section] = [];
-      }
-      data[section].push(user);
-    });
-
-    return {data, sectionIds};
-  },
-  
-  renderSectionHeader(data, sectionId) {
-    var text;
-    return (
-      <View style={styles.sectionHeader}>
-        <Text style={styles.sectionHeaderText}>{sectionId}</Text>
-      </View>
-    );
-  },
-  
-  renderRow(rowData) {
-    return <SampleRow {...rowData} style={styles.row} />
-  },
-
-  render() {
+  render: function() {
     return (
       <ListView
-        ref="listView"
-        automaticallyAdjustContentInsets={false}
         dataSource={this.state.dataSource}
         renderRow={this.renderRow}
-        renderSectionHeader={this.renderSectionHeader}
       />
     );
   },
+  
+  renderRow(data) {
+    return <ItemRow {...itemData} />
+  }
 });
 
 class Project extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.topcontainer}>
-          <Text>Test</Text>
+        
+        <View style={styles.shopContainer}>
+          <Text>Cafe Goes Here</Text>
         </View>
-        <View style={styles.bottomcontainer}>
+    
+        <View style={styles.menuContainer}>
           <ListViewRowsAndSections/>
         </View>
       
-        <Image source={require('./img/items/kopi.png')} style={styles.itemThumbnail} />
       </View>
     );
   }
@@ -136,31 +91,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   itemThumbnail: {
-    width: 50,
-    height: 50,
+    width: 60,
+    height: 60,
   },
-  instructions: {
-    textAlign: 'center',
+  itemTitle: {
     color: '#333333',
     marginBottom: 5,
+    textAlign: 'left',
   },
-  topcontainer: {
+  itemDescription: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'left',
+  },
+  shopContainer: {
     flex: .4,
     alignSelf: 'stretch',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: '#612832',
   },
-  bottomcontainer: {
+  menuContainer: {
     flex: .6,
     alignSelf: 'stretch',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'blue',
+    backgroundColor: '#f4f3f4',
   }
   
 });
